a018c478942354da9d3f05e8404e90fbf53838c7
brahianf <brahianf@unicauca.edu.co>
2021-05-04 05:03:36 -0500

    [add] Actions Redux

---
+++ src/redux/actions/pokeActions.js
@@ -0,0 +1,66 @@
+const URL_API = process.env.API;
+
+export const getDataPoke = () => async (dispatch) => {
+  try {
+    const response = await fetch(`${URL_API}/pokemon?limit=15`);
+    const data = await response.json();
+    data.results.forEach( async (item, index) => {
+      const response = await fetch(`${URL_API}/pokemon/${item.name}`);
+      const data = await response.json();
+      if(index===0){
+        selectedPoke(data);
+      }
+      dispatch({
+        type: 'DATA_ALL_POKE',
+        payload: data,
+      });
+    })
+  } catch (err) {
+    console.log('getAllPoke -> err', err);
+  }
+};
+
+export const selectedPoke = (data) => async (dispatch) => {
+  dispatch({
+    type: 'DATA_BY_ID',
+    payload: data,
+  });
+};
+
+export const getDataPokeById = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(`${URL_API}/pokemon/${id}`);
+    if(response.status === 404){
+      alert('Not Found Pokemon')
+    }
+    const data = await response.json();
+    dispatch({
+      type: 'DATA_BY_ID',
+      payload: data,
+    });
+  } catch (err) {
+    console.log('getPokeById -> err', err);
+  }
+};
+
+export const setFavPoke = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(`${URL_API}/auth/sign-in`, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'mode': 'cors',
+        'Authorization': `Basic ${Buffer.from(`${id}}`).toString('base64')}`,
+      }),
+      body: JSON.stringify(`'apiKeyToken'  ${':'} 'e'`),
+    });
+
+    const data = await response.json();
+    dispatch({
+      type: 'DATA_FAV',
+      payload: data,
+    });
+  } catch (err) {
+    console.log('getFavoritesPoke -> err', err);
+  }
+};
